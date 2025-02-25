9aec6a2379d2a6f1df4e43ff61b387525972643c
Vitória Lima Candido <vitoria.lcandido@gmail.com>
2025-02-25 11:53:33 -0300

    teste de config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    // outras configurações, se necessário
+    // Remova output: 'export'
+    images: {
+      unoptimized: true,
+    },
   }
   
   module.exports = nextConfig
---
+++ out/_next/static/V0IXziWhqVXjXZs-j2RAN/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-b5a0dfa2371b13b3.js"],"/_error":["static/chunks/pages/_error-dfcfa5bb62767c20.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
