3383205edb1721405b60ccce60652a2b5f66b4bc
Vitória Lima Candido <vitoria.lcandido@gmail.com>
2025-06-02 23:08:03 -0300

    Mantém apenas o primeiro e o último commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // Remova output: 'export'
+    images: {
+      unoptimized: true,
+    },
+  }
+  
+  module.exports = nextConfig
